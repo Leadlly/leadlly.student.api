2599973ca2e477ad61dbf680e89c28fc29a66584
shivang-16 <shivangyadav121@gmail.com>
2024-04-05 20:10:24 +0530

    redis added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import { app } from "./app";
 import ConnectToDB from "./db/db";
+import connectToRedis from "./services/redis";
 
 const port = process.env.PORT || 4000
 
+//Database
 ConnectToDB()
+
+//Services
+connectToRedis()
+
 app.listen(port, () => console.log(`server is listening at port ${port}`))
---
+++ src/services/redis.ts
@@ -0,0 +1,17 @@
+import Redis from 'ioredis'
+
+
+export const connectToRedis = () => {
+
+    const redisUri = process.env.REDIS_URI || ''
+    console.log(redisUri)
+    let redis = new Redis(redisUri);
+    redis.on('connect', () => console.log('Redis Connected.'));
+    redis.on('error', (err) => {
+        console.log('Redis Client Error', err);
+        redis.disconnect(); // Disconnect from Redis
+    });
+}
+
+
+export default connectToRedis
