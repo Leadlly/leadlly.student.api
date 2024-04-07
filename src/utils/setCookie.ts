7f4368743a5fc1552e37d9b5f5b8ae617d38bd63
shivang-16 <shivangyadav121@gmail.com>
2024-04-08 01:58:03 +0530

    ADD: nodemailer and jsonwebtoken

---
+++ src/utils/setCookie.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import { CustomError } from "../middlewares/error";
+
+type Cookie = {
+  user: any;
+  res: Response;
+  next: NextFunction;
+  message: string;
+  statusCode: number;
+};
+const setCookie = async ({ user, res, next, message, statusCode }: Cookie) => {
+  try {
+    const secret = process.env.JWT_SECRET;
+    if (secret) {
+      const token = jwt.sign({ id: user._id }, secret);
+
+      res
+        .status(statusCode)
+        .cookie("token", token, {
+          httpOnly: true,
+          maxAge: 30 * 24 * 60 * 60 * 1000,
+          sameSite: "none",
+          secure: true,
+        })
+        .json({
+          success: true,
+          message,
+        });
+    } else {
+      next(new CustomError("Jwt Secret not defined"));
+    }
+  } catch (error: any) {
+    next(new CustomError(error.message));
+  }
+};
+
+export default setCookie;
