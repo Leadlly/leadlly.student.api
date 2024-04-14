81471b1f7b59696623038f2aeaa4da0ba92054a3
shivang-16 <shivangyadav121@gmail.com>
2024-04-14 17:06:26 +0530

    aws and razorpay setup

---
+++ src/services/payment/Razorpay.ts
@@ -0,0 +1,17 @@
+import Razorpay from "razorpay";
+import { config } from 'dotenv';
+config()
+
+const key_id = process.env.RAZORPAY_API_KEY;
+const key_secret = process.env.RAZORPAY_API_SECRET;
+
+if (!key_id || !key_secret) {
+  throw new Error("Razorpay API key or secret is missing");
+}
+
+const razorpay = new Razorpay({
+  key_id: key_id,
+  key_secret: key_secret,
+});
+
+export default razorpay;
