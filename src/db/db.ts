bf4a608e4096449b23e3680b7ec62c11e8fbed18
shivang-16 <shivangyadav121@gmail.com>
2024-04-15 01:24:22 +0530

    setup multiple database connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,20 +1,31 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const ConnectToDB = async() =>{
-    const DatabaseUrl = process.env.DATABASE_URL
+    const DatabaseUrl = process.env.LEADLLY_DB_URL
+    console.log(DatabaseUrl)
     if(!DatabaseUrl){ 
-        console.log("Database url is undefined")
+        console.log("Leadlly_DB url is undefined")
         return
     }
         
     try {
-        await mongoose.connect(DatabaseUrl, {
-             dbName: "leadlly"
-        })
-        console.log('MognoDB Connected.')
+        await mongoose.connect(DatabaseUrl)
+        console.log('Leadlly_DB Connected.')
     } catch (error) {
         console.log(error)
     }
 }
 
+//question_db_connection
+const questions_db_url = process.env.LEADLLY_QUESTIONS_DB_URL;
+
+if (!questions_db_url) {
+  throw new Error("Question_DB url is undefined");
+}
+const questions_db = mongoose.createConnection(questions_db_url);
+
+export {questions_db}
 export default ConnectToDB
