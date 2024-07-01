eadbe45a960ee3d6ddbf620d5329170346c8041c
shivang-16 <shivangyadav121@gmail.com>
2024-07-01 21:52:17 +0530

    updated cors

--- dist/utils/setCookie.js
+++ dist/utils/setCookie.js
@@ -16,8 +16,8 @@ const setCookie = async ({ user, res, next, message, statusCode }) => {
             .cookie("token", token, {
             httpOnly: true,
             maxAge: 30 * 24 * 60 * 60 * 1000,
-            // sameSite: "none",
-            // secure: true,
+            sameSite: "none",
+            secure: true,
         })
             .json({
             success: true,
