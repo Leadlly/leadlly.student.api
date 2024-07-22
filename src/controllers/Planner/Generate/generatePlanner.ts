059ba3540036e0ee42e9f3c7a5df9d9f74abc3fc
shivang-16 <shivangyadav121@gmail.com>
2024-07-22 16:06:28 +0530

    update next week planner

--- src/controllers/Planner/Generate/generatePlanner.ts
+++ src/controllers/Planner/Generate/generatePlanner.ts
@@ -19,7 +19,7 @@ const daysOfWeek = [
 export const generateWeeklyPlanner = async (
   user: IUser,
   backRevisionTopics: IDataSchema[],
-  nextWeek: boolean
+  nextWeek: Boolean
 ) => {
   const activationDate =
     user.freeTrial?.dateOfActivation || user.subscription?.dateOfActivation;
@@ -32,118 +32,228 @@ export const generateWeeklyPlanner = async (
   const activationMoment = moment.tz(activationDate, timezone);
   const currentMoment = moment.tz(timezone);
 
-  let startDate;
+  let startDate: any;
   let endDate;
 
-  // Start from the next day of activation date
-  const nextDay = activationMoment.add(1, 'days').startOf('day');
+  if (nextWeek) {
+    console.log("Generating planner for next week");
 
-  if (nextDay.isSame(currentMoment, 'week')) {
-    startDate = nextDay.toDate();
-    endDate = moment(startDate).endOf("isoWeek").toDate();
-  } else {
-    startDate = currentMoment.startOf("isoWeek").toDate();
-    endDate = moment(startDate).endOf("isoWeek").toDate();
-  }
+    // Calculate the start and end of next week
+    startDate = moment(currentMoment).startOf('isoWeek').add(1, 'week').toDate();
+    endDate = moment(startDate).endOf('isoWeek').toDate();
 
-  const existingPlanner = await Planner.findOne({
-    student: user._id,
-    startDate,
-    endDate,
-  });
 
-  if (existingPlanner) {
-    console.log("Planner already exists");
-    return { message: "Planner already exists", planner: existingPlanner };
-  }
-
-  const yesterday = moment.tz(timezone).subtract(1, "days").startOf("day").toDate();
-
-  const continuousRevisionTopics = (await StudyData.find({
-    user: user._id,
-    tag: "continuous_revision",
-    createdAt: { $gte: yesterday },
-  }).exec()) as IDataSchema[];
-
-  const startDayIndex = nextDay.isSame(currentMoment, 'week')
-    ? daysOfWeek.indexOf(nextDay.format('dddd'))
-    : 0;
-
-  let dailyQuestions;
-  const days = await Promise.all(
-    daysOfWeek.slice(startDayIndex).map(async (day, index) => {
-      const date = nextDay.isSame(currentMoment, 'week')
-        ? moment(nextDay).add(index, "days").toDate()
-        : moment(startDate).add(index, "days").toDate();
-
-      if (day === "Sunday") {
+    const existingPlanner = await Planner.findOne({
+      student: user._id,
+      startDate,
+      endDate,
+    });
+  
+    if (existingPlanner) {
+      console.log("Planner already exists");
+      return { message: "Planner already exists", planner: existingPlanner };
+    }
+  
+    const yesterday = moment.tz(timezone).subtract(1, "days").startOf("day").toDate();
+  
+    const continuousRevisionTopics = (await StudyData.find({
+      user: user._id,
+      tag: "continuous_revision",
+      createdAt: { $gte: yesterday },
+    }).exec()) as IDataSchema[];
+  
+    const startDayIndex = daysOfWeek.indexOf(moment(startDate).format('dddd'));
+  
+    let dailyQuestions;
+    const days = await Promise.all(
+      daysOfWeek.slice(startDayIndex).map(async (day, index) => {
+        const date = moment(startDate).add(index, "days").toDate();
+  
+        if (day === "Sunday") {
+          return {
+            day,
+            date,
+            continuousRevisionTopics: [],
+            backRevisionTopics: [],
+            questions: [],
+          };
+        }
+  
+        const { dailyContinuousTopics, dailyBackTopics } = getDailyTopics(
+          [],
+          backRevisionTopics,
+          user,
+        );
+  
+        const dailyTopics = [...dailyContinuousTopics, ...dailyBackTopics];
+  
+        // Fetch and update topics in the StudyData collection
+        await Promise.all(dailyTopics.map(async (data) => {
+          const studyData = await StudyData.findOne({
+            user: user._id,
+            "topic.name": data.topic.name
+          }).exec() as IDataSchema;
+  
+          if (studyData) {
+            if (!studyData.topic.studiedAt) {
+              studyData.topic.studiedAt = [];
+            }
+            studyData.topic.studiedAt.push({ date, efficiency: 0 });
+  
+            if (!studyData.topic.plannerFrequency) {
+              studyData.topic.plannerFrequency = 0;
+            }
+            studyData.topic.plannerFrequency += 1;
+  
+            if (studyData.tag === "unrevised_topic") {
+              studyData.tag = "active_unrevised_topic";
+            }
+  
+            await studyData.save();
+          }
+        }));
+  
+        dailyQuestions = await getDailyQuestions(day, date, dailyTopics);
         return {
           day,
           date,
-          continuousRevisionTopics: [],
-          backRevisionTopics: [],
-          questions: [],
+          continuousRevisionTopics: dailyContinuousTopics,
+          backRevisionTopics: dailyBackTopics,
+          questions: dailyQuestions,
         };
-      }
+      }),
+    );
+  
+    const generatedPlanner = new Planner({
+      student: user._id,
+      startDate,
+      endDate,
+      days,
+    });
+  
+    const planner = await Planner.create(generatedPlanner);
+  
+    continuousRevisionTopics.forEach(
+      (data) => (data.tag = "active_continuous_revision"),
+    );
+    await Promise.all(continuousRevisionTopics.map((data) => data.save()));
+  
+    return { message: "Planner created", planner };
 
-      const { dailyContinuousTopics, dailyBackTopics } = getDailyTopics(
-        [],
-        backRevisionTopics,
-        user,
-      );
+  } else {
+    console.log("Generating planner for current week");
 
-      const dailyTopics = [...dailyContinuousTopics, ...dailyBackTopics];
+    // Start from the next day of activation date
+    const nextDay = activationMoment.add(1, 'days').startOf('day');
 
-      // Fetch and update topics in the StudyData collection
-      await Promise.all(dailyTopics.map(async (data) => {
-        const studyData = await StudyData.findOne({
-          user: user._id,
-          "topic.name": data.topic.name
-        }).exec() as IDataSchema;
+    if (nextDay.isSame(currentMoment, 'week')) {
+      startDate = nextDay.toDate();
+      endDate = moment(startDate).endOf("isoWeek").toDate();
+    } else {
+      startDate = moment(nextDay).startOf('isoWeek').add(1, 'weeks').toDate();
+      endDate = moment(startDate).endOf('isoWeek').toDate();
+    }
 
-        if (studyData) {
-          if (!studyData.topic.studiedAt) {
-            studyData.topic.studiedAt = [];
-          }
-          studyData.topic.studiedAt.push({ date, efficiency: 0 });
 
-          if (!studyData.topic.plannerFrequency) {
-            studyData.topic.plannerFrequency = 0;
-          }
-          studyData.topic.plannerFrequency += 1;
+    const existingPlanner = await Planner.findOne({
+      student: user._id,
+      startDate,
+      endDate,
+    });
+  
+    if (existingPlanner) {
+      console.log("Planner already exists");
+      return { message: "Planner already exists", planner: existingPlanner };
+    }
+  
+    const yesterday = moment.tz(timezone).subtract(1, "days").startOf("day").toDate();
+  
+    const continuousRevisionTopics = (await StudyData.find({
+      user: user._id,
+      tag: "continuous_revision",
+      createdAt: { $gte: yesterday },
+    }).exec()) as IDataSchema[];
+  
+    const startDayIndex = nextDay.isSame(currentMoment, 'week')
+    ? daysOfWeek.indexOf(nextDay.format('dddd'))
+    : 0;
 
-          if (studyData.tag === "unrevised_topic") {
-            studyData.tag = "active_unrevised_topic";
-          }
-
-          await studyData.save();
+    let dailyQuestions;
+    const days = await Promise.all(
+     daysOfWeek.slice(startDayIndex).map(async (day, index) => {
+      const date = nextDay.isSame(currentMoment, 'week')
+        ? moment(nextDay).add(index, "days").toDate()
+        : moment(startDate).add(index, "days").toDate();
+  
+        if (day === "Sunday") {
+          return {
+            day,
+            date,
+            continuousRevisionTopics: [],
+            backRevisionTopics: [],
+            questions: [],
+          };
         }
-      }));
-
-      dailyQuestions = await getDailyQuestions(day, date, dailyTopics);
-      return {
-        day,
-        date,
-        continuousRevisionTopics: dailyContinuousTopics,
-        backRevisionTopics: dailyBackTopics,
-        questions: dailyQuestions,
-      };
-    }),
-  );
-
-  const generatedPlanner = new Planner({
-    student: user._id,
-    startDate,
-    endDate,
-    days,
-  });
-
-  const planner = await Planner.create(generatedPlanner);
-
-  continuousRevisionTopics.forEach(
-    (data) => (data.tag = "active_continuous_revision"),
-  );
-  await Promise.all(continuousRevisionTopics.map((data) => data.save()));
-
-  return { message: "Planner created", planner };
+  
+        const { dailyContinuousTopics, dailyBackTopics } = getDailyTopics(
+          [],
+          backRevisionTopics,
+          user,
+        );
+  
+        const dailyTopics = [...dailyContinuousTopics, ...dailyBackTopics];
+  
+        // Fetch and update topics in the StudyData collection
+        await Promise.all(dailyTopics.map(async (data) => {
+          const studyData = await StudyData.findOne({
+            user: user._id,
+            "topic.name": data.topic.name
+          }).exec() as IDataSchema;
+  
+          if (studyData) {
+            if (!studyData.topic.studiedAt) {
+              studyData.topic.studiedAt = [];
+            }
+            studyData.topic.studiedAt.push({ date, efficiency: 0 });
+  
+            if (!studyData.topic.plannerFrequency) {
+              studyData.topic.plannerFrequency = 0;
+            }
+            studyData.topic.plannerFrequency += 1;
+  
+            if (studyData.tag === "unrevised_topic") {
+              studyData.tag = "active_unrevised_topic";
+            }
+  
+            await studyData.save();
+          }
+        }));
+  
+        dailyQuestions = await getDailyQuestions(day, date, dailyTopics);
+        return {
+          day,
+          date,
+          continuousRevisionTopics: dailyContinuousTopics,
+          backRevisionTopics: dailyBackTopics,
+          questions: dailyQuestions,
+        };
+      }),
+    );
+  
+    const generatedPlanner = new Planner({
+      student: user._id,
+      startDate,
+      endDate,
+      days,
+    });
+  
+    const planner = await Planner.create(generatedPlanner);
+  
+    continuousRevisionTopics.forEach(
+      (data) => (data.tag = "active_continuous_revision"),
+    );
+    await Promise.all(continuousRevisionTopics.map((data) => data.save()));
+  
+    return { message: "Planner created", planner };
+  }
 };
