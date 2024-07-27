58c67c2708197d4627d1226689a68267ed012fbc
adil-khursheed <adilkhursheed60@gmail.com>
2024-07-27 18:53:36 +0530

    implemented weekly quiz controller

--- src/routes/quiz.ts
+++ src/routes/quiz.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { checkAuth } from "../middlewares/checkAuth";
 import { saveDailyQuiz } from "../controllers/Quiz/DailyQuiz";
+import { createWeeklyQuiz } from "../controllers/Quiz/WeeklyQuiz";
 
 const router = express.Router();
 
 router.post("/save", checkAuth, saveDailyQuiz);
+router.post("/create/weekly", checkAuth, createWeeklyQuiz);
 
 export default router;
