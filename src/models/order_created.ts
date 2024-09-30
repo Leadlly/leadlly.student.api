99083eb118c8a519da6b23e466eb476c47ffd90d
adil-khursheed <adilkhursheed60@gmail.com>
2024-09-30 19:21:39 +0530

    fix: fixed typos in order model

--- src/models/order_created.ts
+++ src/models/order_created.ts
@@ -1,23 +1,28 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IOrder extends Document {
-  user: mongoose.Types.ObjectId; 
-  order_id: string; 
-  planId: string; 
-  duration: number; 
-  categogy: 'basic' | 'pro' | 'premium' | 'free' | null;
-  coupon?: string; 
-  createdAt: Date
+  user: mongoose.Types.ObjectId;
+  order_id: string;
+  planId: string;
+  duration: number;
+  category: "basic" | "pro" | "premium" | "free" | null;
+  coupon?: string;
+  createdAt: Date;
 }
 
 const OrderSchema: Schema = new Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
   order_id: { type: String, required: true },
   planId: { type: String },
   duration: { type: Number },
-  category: { type: String, enum: [ 'basic', 'pro', 'premium', 'free'  ]},
+  category: { type: String, enum: ["basic", "pro", "premium", "free"] },
   coupon: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
-export const Order = mongoose.model<IOrder>('Order', OrderSchema);
+export const Order = mongoose.model<IOrder>("Order", OrderSchema);
