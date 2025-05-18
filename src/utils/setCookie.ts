630358e1592b80c174a95c954ba2c0930bc13a28
shivang-16 <shivangyadav121@gmail.com>
2025-05-19 00:48:19 +0530

    fix cookie

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -22,8 +22,8 @@ const setCookie = async ({ user, res, next, message, statusCode }: Cookie) => {
       .cookie("token", token, {
         httpOnly: true,
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-        sameSite: "lax",
-        secure: false,
+        sameSite: "none",
+        secure: true,
       })
       .json({
         success: true,
